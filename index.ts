a553933b54f965a3f4df0aa74c559191d2a3769d
ATUHAIRE JUDE INNOCENT <judeinno@gmail.com.com>
2019-09-04 10:14:13 +0300

    Show explicit types

--- index.ts
+++ index.ts
@@ -1,5 +1,12 @@
+// Explicit type
+const arrayExample: number[] = [1, 3, 4, 5];
+
 let example1: boolean | number = 35;
 
-let example2: number = 35;
+let example2 = arrayExample.reduce((num1, num2) => num1 + num2);
 
-let example3: string = 'Jude';
+let example3: string = 'Hello world';
+
+let example4: undefined = undefined;
+
+let example5: null = null;
