795b1fe0ab606259e8a5b6f8c5164c15b1ea570b
ATUHAIRE JUDE INNOCENT <judeinno@gmail.com.com>
2019-09-04 14:04:20 +0300

    Checking types

--- index.ts
+++ index.ts
@@ -1,12 +1,7 @@
-// Explicit type
-const arrayExample: number[] = [1, 3, 4, 5];
+import { Bear } from './bear.model';
 
-let example1: boolean | number = 35;
+const bear = new Bear(3);
 
-let example2 = arrayExample.reduce((num1, num2) => num1 + num2);
-
-let example3: string = 'Hello world';
-
-let example4: undefined = undefined;
-
-let example5: null = null;
+if (bear instanceof Bear) {
+    console.log("Hello from TypeScript");
+}
