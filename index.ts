520958324556da4165d4d1fbdac682d31e2d833c
ATUHAIRE JUDE INNOCENT <judeinno@gmail.com.com>
2019-09-04 18:16:49 +0300

    Working with generics

--- index.ts
+++ index.ts
@@ -1,12 +1,5 @@
-import { Bear, Man, Pig } from './interfaces';
+function example<T>(arg: T): T {
+  return arg
+}
 
-let man: Man | Pig;
-let bear: Bear;
-let pig: Pig;
-
-type ManBearPig = Bear & Man & Pig
-
-let manBearPig: ManBearPig ;
-manBearPig.firstName = 'Dylan';
-manBearPig.claws = 3;
-manBearPig.bacon = false;
+example(5);
