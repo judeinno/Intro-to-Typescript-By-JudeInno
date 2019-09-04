1b4f1334d5a1ad6175ec11dc16304ee2b6549d0c
ATUHAIRE JUDE INNOCENT <judeinno@gmail.com.com>
2019-09-04 18:03:07 +0300

    Working with barrels

--- index.ts
+++ index.ts
@@ -1,9 +1,1 @@
-type person = {firstName: string}
-
-const example1: object = undefined;
-
-const example2: Object = NaN;
-
-const example3: person = {firstName: 'Jude'};
-
-example3.firstName = 'Dylan';
+import { Bear, Man, Pig } from './interfaces';
