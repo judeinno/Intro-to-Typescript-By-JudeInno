3d3135b7c961f1080f19e0487d2d50b3f9977588
ATUHAIRE JUDE INNOCENT <judeinno@gmail.com.com>
2019-09-04 18:12:20 +0300

    Working with intersection types

--- index.ts
+++ index.ts
@@ -1,1 +1,12 @@
 import { Bear, Man, Pig } from './interfaces';
+
+let man: Man | Pig;
+let bear: Bear;
+let pig: Pig;
+
+type ManBearPig = Bear & Man & Pig
+
+let manBearPig: ManBearPig ;
+manBearPig.firstName = 'Dylan';
+manBearPig.claws = 3;
+manBearPig.bacon = false;
